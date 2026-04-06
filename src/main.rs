8a2ce51d41578bc144a5c320ee1fe4a8c4d624e0
agavalda <agavalda@digital.ai>
2026-04-06 16:00:37 +0200

    read entries file from argument

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
 use crate::entry::parse_entries_from_file;
 use crate::html::create_html;
+use std::env;
 
 mod entry;
 mod html;
 
 fn main() {
+    let args: Vec<String> = env::args().collect();
+    let events_path = args[1].as_str();
     let title = "My count of something"; // TODO get this from config file
-    let entries = parse_entries_from_file("example-events/events.txt"); // TODO get this from config file
+    let entries = parse_entries_from_file(events_path);
     let html = create_html(&entries, title);
     std::fs::write("output/index.html", html).expect("Failed to write HTML file");
 }
